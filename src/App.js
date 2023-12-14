fda79072fe86717f2ca849c2d481284138f1b0d1
Vishal kumar <vishal.kumar@trigvent.com>
2023-12-14 13:32:35 +0530

    init

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <iframe src="https://gen.sendtric.com/countdown/ohkfod1wkk" name="myIFrame" style="border: solid #000000;"></iframe>
+    <iframe src="https://gen.sendtric.com/countdown/ohkfod1wkk" name="myIFrame"></iframe>
   );
 }
 
