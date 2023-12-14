afcaa8580f34f801ceaac993722bc49602f881cb
Vishal kumar <vishal.kumar@trigvent.com>
2023-12-14 12:56:53 +0530

    init

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 function App() {
   return (
-    <div>Trigvent Test</div>
+    <iframe src="https://gen.sendtric.com/countdown/ohkfod1wkk" name="myIFrame" style="border: solid #000000;"></iframe>
   );
 }
 
